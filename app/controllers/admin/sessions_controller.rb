67171c412986e44d6e68a7bd840c62726ec6c987
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2023-01-01 13:52:15 +0000

    [管理者ログアウト機能]

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -7,4 +7,22 @@ class Admin::SessionsController < Admin::Base
       render action: "new"
     end  
   end  
+  
+  def create
+    @form = Admin::LoginForm.new(params[:admin_login_form])
+    if @form.email.prsent?
+      admin_member = AdminMember.find_by("LOWER(email) =?", @form.email.downcase)
+    end
+    if admin_member
+      session[:admin_member_id] = admin_member.id
+      redirect_to :admin_root
+    else
+      render action: "new"
+    end
+    
+    def destroy
+      session.delete(admin_member_id)
+      redirect_to :admin_root
+    end 
+  end
 end
